c6a2f811d7302b3230161ff0236fc74350b98734
FrancklinJr <francklinjunior@outlook.com>
2024-08-23 20:31:08 -0300

    alteração para corrida de 600km entre 2 carros

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 from frota import *
 
 if __name__ == "__main__":
-    print('Cadastre um carro')
+    print('Cadastre o carro 1')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
 
-    kms = float(input('Digite com quantos Kms: '))
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0.0, motor = False)
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = True)
+    print("Cadastre o carro 2")
+    nm_modelo = input('Digite o modelo: ')
+    nm_marca = input('Digite a marca: ')
+    nm_cor = input('Digite a cor: ')
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0.0, motor = False)
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 10000:
+    while carro1.odometro < 600 and carro2.odometro < 600:
         try:
             print('1- Ligar motor')
             print('2- Desligar motor')
@@ -25,20 +29,36 @@ if __name__ == "__main__":
 
             if op == 1:
                 carro1.ligar()
+                carro2.ligar()
             elif op == 2:
                 carro1.desligar()
+                carro2.desligar()
             elif op == 3:
-                v = float(input("Informe a velocidade: "))
-                t = float(input("Informe o tempo: "))
-                carro1.acelerar(v, t)
+                v1 = float(input("Informe a velocidade do primeiro carro: "))
+                t1 = float(input("Informe o tempo do primeiro carro: "))
+                carro1.acelerar(v1, t1)
+                v2 = float(input("Informe a velocidade do segundo carro: "))
+                t2 = float(input("Informe o tempo do segundo carro: "))
+                carro2.acelerar(v2, t2)
+
 
             print('Infos atuais do carro')
             print(carro1)
+            print(carro2)
         except Exception as e:
             print("Erro!")
             print(e)
 
+
     carro1.desligar()
+    carro2.desligar()
+    if carro1.odometro > carro2.odometro:
+        print("o carro 1 ganhou!")
+    elif carro2.odometro > carro1.odometro:
+        print("o carro 2 ganhou!")
+    else:
+        print("empate!")
     print(carro1)
-    print('Parar para trocar óleo!!!')
+    print(carro2)
+
 
